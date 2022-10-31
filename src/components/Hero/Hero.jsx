20b746fb989ee1311207b1c525b18b411da6c7ab
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-11-01 01:57:07 +0530

    Added Framer Motion to the background image in hero section also

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './hero.css';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
     <div className="hero bg-[#131314] mt-[-90px] h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+        <motion.div
+            initial = {{
+                opacity: 0
+            }}
+            animate = {{
+                opacity: [0, 0.7, 1]
+            }}
+            transition = {{
+                duration: 1
+            }}
+            className="image h-screen w-screen absolute">
+        </motion.div>
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
