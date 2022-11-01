70ace51a4b277a7535fd6548bf7d529460d4e30f
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-11-01 10:08:46 +0530

    Completing Animation for the loader page and adding cursor follower

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import './hero.css';
 import { motion } from 'framer-motion';
+import logo from '../../assets/logo-no-background.png'
+// import Cursor from 'react-cursor-follow';
+import AnimatedCursor from "react-animated-cursor";
 
 const Hero = () => {
   return (
+    <>
+    <AnimatedCursor
+      innerSize={13}
+      outerSize={13}
+      color='193, 11, 111'
+      outerAlpha={0.3}
+      innerScale={0.7}
+      outerScale={4}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]}
+    />
     <div className="hero bg-[#131314] mt-[-90px] h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+        {/* <Cursor size={45} duration={0.6} hollow={true} pulse={true} color="#4b0082" /> */}
         <motion.div
             initial = {{
                 opacity: 0
@@ -17,20 +43,9 @@ const Hero = () => {
             }}
             className="image h-screen w-screen absolute">
         </motion.div>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="z-10 m-0 w-[200px] md:w-[350px]" alt="" />
     </div>
+    </>
   )
 }
 
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './loader.css';
 import { motion } from 'framer-motion';
+import logo from '../../assets/logo-no-background.png'
 
 const Loader = () => {
   return (
-    <div className='loader flex flex-col h-screen bg-[#131314]'>
+    <div className='loader flex flex-col justify-center items-center h-screen bg-[#131314]'>
         <motion.div
             initial = {{
                 opacity: 0.1
@@ -13,10 +14,25 @@ const Loader = () => {
                 opacity: [0.1, 0.4, 0.2, 0.11, 0.05, 0]
             }}
             transition = {{
-                duration: 0.6
+                duration: 0.4
             }}
-            className="image h-screen">
+            className="image h-screen w-screen absolute">
         </motion.div>
+        <motion.img
+        initial = {{
+            opacity: 0
+        }}
+        animate = {{
+            // opacity: [0, 0.2, 0.4, 0.6, 0.8, 0.9, 1, 0.9, 0.8, 0.6, 0.4, 0.2, 0]
+            // opacity: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
+            opacity: [0, 1, 0]
+        }}
+        transition = {{
+            duration: 1.5,
+            delay: 0.4,
+            type: 'spring'
+        }}
+        className='mt-[33px] w-[120px] md:w-[200px]' src={logo} alt="" />
     </div>
   )
 }
